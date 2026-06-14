760b4227848c0fa3a244c4bce4b0ee1f6ad68976
miyushan <miyushanrodrigo@gmail.com>
2026-06-14 13:27:57 +0530

    feat: add DTOs

---
+++ DTOs/MoverDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FinTrack.DTOs
+{
+    public class MoverDto
+    {
+        public string Ticker { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal ChangeAmount { get; set; }
+        public string ChangePercentage { get; set; } = null!;
+        public long Volume { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
---
+++ DTOs/TopMoverDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinTrack.DTOs
+{
+    public class TopMoverDto
+    {
+        public List<MoverDto> TopGainers { get; set; } = new();
+        public List<MoverDto> TopLosers { get; set; } = new();
+        public List<MoverDto> MostActivelyTraded { get; set; } = new();
+    }
+}
